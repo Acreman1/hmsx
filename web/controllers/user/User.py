d7e2dd5fd34b7b37bafbbefd33b39ceafbc51453
Acreman <acreman@sina.com>
2020-04-23 10:28:17 +0800

    login.js

--- web/controllers/user/User.py
+++ web/controllers/user/User.py
@@ -1,8 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 
 router_user = Blueprint('user_page',__name__)
 
 @router_user.route("/login")
 def login():
-    return "login"
+    return render_template("user/login.html")
+
+@router_user.route("/logout")
+def logout():
+    return "logput"
+
+@router_user.route("/edit")
+def edit():
+    return "edit"
+
+@router_user.route("/rest-pwd")
+def restPwd():
+    return "rest-pwd"
